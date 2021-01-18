c48785618e9cbe3931b4472fb109334711bb065f
Restyled.io <commits@restyled.io>
2021-01-18 13:02:10 +0000

    Restyled by prettier

--- assets/stars.js
+++ assets/stars.js
@@ -1,17 +1,19 @@
 // star creation
 
-const animatedSection = document.querySelector('.animated-area');
+const animatedSection = document.querySelector(".animated-area");
 
 function createStars() {
-  const star = document.createElement('span');
-  star.classList.add('star');
+  const star = document.createElement("span");
+  star.classList.add("star");
 
-  star.style.left = Math.random() * (innerWidth + 50) + 'px';
-  star.style.top = Math.floor(Math.random() * 10) + 20 + 'px';
+  star.style.left = Math.random() * (innerWidth + 50) + "px";
+  star.style.top = Math.floor(Math.random() * 10) + 20 + "px";
 
   animatedSection.append(star);
 
-  setTimeout(() => { star.remove(); }, 4000);
+  setTimeout(() => {
+    star.remove();
+  }, 4000);
 }
 
 setInterval(createStars, 10);
