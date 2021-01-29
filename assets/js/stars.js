6dc11651028a8521de9668e1b7fd293a8d471b1e
tusharnankani <tusharnankani3@gmail.com>
2021-01-29 22:47:22 +0530

    :star: feat: add star, randomize transitions

--- assets/js/stars.js
+++ assets/js/stars.js
@@ -2,18 +2,33 @@
 
 const animatedSection = document.querySelector(".animated-area");
 
+
+// star creation
 function createStars() {
-  const star = document.createElement("span");
-  star.classList.add("star");
+  let i;
+  for(i = 0; i < 70; ++i)
+  {
+      // creation of stars using --> span.star
+      const star = document.createElement("span");
+      star.classList.add("star");
 
-  star.style.left = Math.random() * (innerWidth + 50) + "px";
-  star.style.top = Math.floor(Math.random() * 10) + 20 + "px";
+      var 
+      x = Math.floor(Math.random() * window.innerWidth), 
+      y = Math.floor(Math.random() * window.innerHeight),
+      duration = Math.random() * 10,
+      size = Math.random() * 2,
+      z = Math.floor(Math.random() * 1) + 2;
 
-  animatedSection.append(star);
+      star.style.left = x + 'px';
+      star.style.top = y + 'px';
+      star.style.width = z + size + 'px';
+      star.style.height = z  + size + 'px';
 
-  setTimeout(() => {
-    star.remove();
-  }, 4000);
+      star.style.animationDuration = 5 + duration + 's'; 
+      star.style.animationDelay = duration + 's'; 
+
+      animatedSection.append(star);
+  }
 }
 
-setInterval(createStars, 10);
+createStars();
